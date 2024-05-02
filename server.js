31384f2354584d55b4185105b0b7c1a1932ee0e2
edupoly <info.edupoly@gmail.com>
2024-05-02 07:29:06 +0530

    sdfh

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+var jwt = require('jsonwebtoken');
+var bodyParser=require('body-parser');
+server.use(bodyParser.json())
+server.use(middlewares)
+server.use((req, res, next) => {
+    console.log(req.headers)
+    if(req.headers.token){
+        next();
+    }
+    else{
+        if(req.body.username){
+            var token = jwt.sign(req.body, 'edosecretkey');
+            res.header('X-Hello', 'World')
+            res.header('token',token)
+            res.json({msg:"loginsuccess",token})
+        }
+        else{
+            res.json({msg:'pleaselogin'})
+        }
+    }
+
+})
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
